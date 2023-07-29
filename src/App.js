ad637c38f038f6b2fc2462addfd6f549c9ce9118
opsingh861 <62853140+opsingh861@users.noreply.github.com>
2023-07-29 23:45:12 +0530

    Banner section completed

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 
 import './App.css';
-import "slick-carousel/slick/slick.css"; 
+
+//importing slick css
+import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+
+// importing routes
+import { Routes, Route } from 'react-router-dom';
+
+// pages
+import HomePage from './pages/Home.page';
+import MoviePage from './pages/Movie.page';
+import PlayPage from './pages/Play.page';
 
 function App() {
   return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-    </div>
+    <Routes>
+      <Route path='/' element={<HomePage />} />
+      <Route path='movie/:id' element={<MoviePage />} />
+      <Route path='plays' element={<PlayPage />} />
+    </Routes>
   );
 }
 
