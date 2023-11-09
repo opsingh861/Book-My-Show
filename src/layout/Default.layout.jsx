daa376c911955ba5e2e364acb44ba192a44d3282
Aditya Dhanraj <opsingh861@gmail.com>
2023-11-10 00:16:30 +0530

    removed footer text

--- src/layout/Default.layout.jsx
+++ src/layout/Default.layout.jsx
@@ -6,7 +6,7 @@ const DefaultLayoutHoc = (Component) => ({ ...props }) => {
         <div>
             <Navbar />
             <Component {...props} />
-            <div>Footer</div>
+            {/* <div>Footer</div> */}
         </div>
     )
 }
--- src/layout/Movie.layout.jsx
+++ src/layout/Movie.layout.jsx
@@ -6,7 +6,7 @@ const MovieLayoutHoc = (Component) => ({ ...props }) => {
         <div>
             <MovieNavbar />
             <Component {...props} />
-            <div>Footer</div>
+            {/* <div>Footer</div> */}
         </div>
     )
 }
